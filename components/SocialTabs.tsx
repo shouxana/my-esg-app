27ce67f4b18319d0b4b0b74070177fd5f3c6864e
Milos Marjanovic <marjanovicmilos1803@gmail.com>
2024-12-13 23:20:16 +0100

    Nastavak

--- components/SocialTabs.tsx
+++ components/SocialTabs.tsx
@@ -414,30 +414,22 @@ const SocialTabs: React.FC<SocialTabsProps> = ({ company }) => {
       </div>
 
        {/* Tab Content */}
-      <div className="mt-4">
-        {activeTab === 'input' && company && (
-          <DataInputForm company={company} />
-        )}
-        {activeTab === 'visuals' && !isLoading && years.length > 0 && company && (
-          <div className="space-y-6">
-            <EducationChart company={company} />
-            <GenderDistributionChart years={years} company={company} />
-            <EmployeeFluctuationChart company={company} />
-            <LeaveTrackingChart company={company} />
-          </div>
-        )}
-        {activeTab === 'visuals' && isLoading && (
-          <div className="h-64 flex items-center justify-center text-gray-500">
-            Loading data...
-          </div>
-        )}
-        {activeTab === 'visuals' && !isLoading && years.length === 0 && (
-          <div className="h-64 flex items-center justify-center text-gray-500">
-            No data available for {company.toUpperCase()}
-          </div>
-        )}
-        
-      </div>
+<div className="mt-4">
+  {activeTab === 'input' && company && (
+    <DataInputForm company={company} />
+  )}
+  {activeTab === 'visuals' && (
+    <div className="space-y-6">
+      <EducationChart company={company} />
+      <GenderDistributionChart years={years} company={company} />
+      <EmployeeFluctuationChart company={company} />
+      <LeaveTrackingChart company={company} />
+    </div>
+  )}
+  {activeTab === 'pdfs' && (
+    <PDFManagement />
+  )}
+</div>
   </div>
 );
 };
